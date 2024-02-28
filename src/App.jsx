408f47ee321bc29dc87b90e2444c2b051e4e668a
Abdalrahman-Muhammad <abdelrhmanmohamed421@gmail.com>
2024-02-28 17:36:15 +0200

    add data file

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,12 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 import { Form } from './components/Form';
 import { Logo } from './components/Logo';
 import { PackingList } from './components/PackingList';
 import { Stats } from './components/Stats';
+import data from '../public/data.js';
 
 export default function App() {
-  const [items, setItems] = useState([]);
-  useEffect(() => {
-    const fetchData = async () => {
-      const res = await fetch('../public/data.json');
-      const data = await res.json();
-      setItems(data);
-    };
-    fetchData();
-  }, []);
+  const [items, setItems] = useState(data);
 
   return (
     <div className='app'>
